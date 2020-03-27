9302d992f5de7075d361e2d11c9712f5fd4bf6a7
glcoder0 <glcoder@mail.com>
2020-03-27 10:27:52 +0000

    split by of aswell and sorted reduced fragments for maximum frequency first

--- reduce_fragments.py
+++ reduce_fragments.py
@@ -6,7 +6,8 @@ with open("label_fragments.txt") as fp:
 		if line in frags: frags[line]+=1 
 		else: frags[line]=1
 
-sorted_frags = {k: v for k, v in sorted(frags.items(), key=lambda item: item[1])}
-for k,v in sorted_frags.items():
-	print(str(k))
+sorted_frags = {k: v for k, v in sorted(frags.items(), key=lambda item: -item[1])}
+unique_frags=[k for k,v in sorted_frags.items()]
+for item in unique_frags:
+	print(item)
 
